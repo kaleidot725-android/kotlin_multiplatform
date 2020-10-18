fd35407bdcec0fb54918bc439684f93d193ac556
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-10-18 10:15:18 -0500

    fix: sql delight versions

--- src/shared/build.gradle.kts
+++ src/shared/build.gradle.kts
@@ -8,9 +8,9 @@ plugins {
 }
 
 object SqlDelight {
-    const val runtime = "com.squareup.sqldelight:runtime:$"
-    const val android = "com.squareup.sqldelight:android-driver:$"
-    const val native = "com.squareup.sqldelight:native-driver:$
+    const val runtime = "com.squareup.sqldelight:runtime:1.4.4"
+    const val android = "com.squareup.sqldelight:android-driver:1.4.4"
+    const val native = "com.squareup.sqldelight:native-driver:1.4.4"
 }
 
 group = "jp.kaleidot725.sample"
@@ -33,7 +33,12 @@ kotlin {
         }
     }
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(SqlDelight.runtime)
+            }
+        }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -43,7 +48,7 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 implementation("com.google.android.material:material:1.2.0")
-                implementation("com.squareup.sqldelight:android-driver:1.2.0")
+                implementation(SqlDelight.android)
             }
         }
         val androidTest by getting {
@@ -54,12 +59,13 @@ kotlin {
         }
         val iosMain by getting {
             dependencies {
-                implementation("com.squareup.sqldelight:ios-driver:1.2.0")
+                implementation(SqlDelight.native)
             }
         }
         val iosTest by getting
     }
 }
+
 android {
     compileSdkVersion(29)
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
@@ -75,6 +81,7 @@ android {
         }
     }
 }
+
 val packForXcode by tasks.creating(Sync::class) {
     group = "build"
     val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
@@ -87,4 +94,5 @@ val packForXcode by tasks.creating(Sync::class) {
     from({ framework.outputDirectory })
     into(targetDir)
 }
+
 tasks.getByName("build").dependsOn(packForXcode)
